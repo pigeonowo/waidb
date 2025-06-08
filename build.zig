1dc717cbd32d1a31dfb8461efb32c9285af33a0c
pigeonowo <jonas.stiller@t-online.de>
2025-06-08 23:06:47 +0200

    Put main.zig into its own folder as the repl

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/repl/main.zig"),
         .target = target,
         .optimize = optimize,
     });
