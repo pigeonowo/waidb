09f904fc2c15ec3752f6a24f2a4117134b10a48f
pigeonowo <jonas.stiller@t-online.de>
2025-06-09 16:40:20 +0200

    Add DB and start with Tables

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,62 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+// +----- Database -----+
+
+const DBError = std.fs.Dir.OpenError || std.fs.Dir.MakeError;
+
+const DB = struct {
+    const Self = @This();
+    dir: std.fs.Dir,
+
+    /// Creates or Updates a table using a custom Struct.
+    fn table(self: *Self, comptime T: type) Table(T) {
+        const tbl = Table(T).init(self);
+        if (try tbl.exists() and try tbl.table_changed()) {
+            try tbl.replace_table();
+        } else if (!tbl.table_exists()) {
+            try tbl.create_table();
+        }
+        return tbl;
+    }
+};
+
+/// Opens a Database directory.
+/// Creates the directory if it doesn't exist
+///
+/// args:
+/// - name: []u8 `Directory path to database directory from current directory`
+pub fn open(name: []u8) DBError!DB {
+    const cur_dir = std.fs.cwd();
+    const db_dir = cur_dir.openDir(name, .{}) catch mkdir: {
+        try cur_dir.makeDir(name);
+        break :mkdir try cur_dir.openDir(name, .{});
+    };
+    return DB{ .dir = db_dir };
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+// +----- TABLES -----+
+// Tabbles are single files under a Database directory.
+
+const TableError = error{noDatabase};
+
+pub fn Table(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        type: type,
+        db: *DB,
+
+        /// Initializes the table with a corresponding Database.
+        /// If no Database is available, no tables can be found.
+        fn init(db: *DB) Self {
+            return Self{ .type = T, .db = db };
+        }
+        fn exists(self: *Self) TableError!bool {
+            self.db.dir.access(@typeName(self.type), .{ .mode = .read_only }) catch return false;
+            return true;
+        }
+        fn changed(self: *Self) !bool {}
+        fn replace(self: *Self) !void {}
+        fn create(self: *Self) !void {}
+    };
 }
