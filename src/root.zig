a74b499049c8df4684c572e55120410e8befbab2
pigeonowo <jonas.stiller@t-online.de>
2025-06-09 20:19:21 +0200

    Open Database directory and create Tables with headers

--- src/root.zig
+++ src/root.zig
@@ -10,12 +10,12 @@ const DB = struct {
     dir: std.fs.Dir,
 
     /// Creates or Updates a table using a custom Struct.
-    fn table(self: *Self, comptime T: type) Table(T) {
-        const tbl = Table(T).init(self);
-        if (try tbl.exists() and try tbl.table_changed()) {
-            try tbl.replace_table();
-        } else if (!tbl.table_exists()) {
-            try tbl.create_table();
+    pub fn table(self: *Self, comptime T: type) !Table(T) {
+        var tbl = Table(T).init(self);
+        if (try tbl.exists() and try tbl.changed()) {
+            try tbl.replace();
+        } else if (!try tbl.exists()) {
+            try tbl.create();
         }
         return tbl;
     }
@@ -26,7 +26,7 @@ const DB = struct {
 ///
 /// args:
 /// - name: []u8 `Directory path to database directory from current directory`
-pub fn open(name: []u8) DbError!DB {
+pub fn open(name: []const u8) DbError!DB {
     const cur_dir = std.fs.cwd();
     const db_dir = cur_dir.openDir(name, .{}) catch mkdir: {
         try cur_dir.makeDir(name);
@@ -43,53 +43,54 @@ const TableError = error{noDatabase};
 pub fn Table(comptime T: type) type {
     // validation
     const struct_fields = @typeInfo(T).@"struct".fields;
-    if (struct_fields.len > 255) @panic("Tables can't have more than 255 fields.");
-    for (struct_fields) |f| if (f.name.len > 255) @panic("Table fields can't have more than 255 characters");
+    comptime if (struct_fields.len > 255) @panic("Tables can't have more than 255 fields.");
+    comptime for (struct_fields) |f| if (f.name.len > 255) @panic("Table fields can't have more than 255 characters");
     // Table Struct
     return struct {
         const Self = @This();
-        type: type,
         db: *DB,
 
         /// Initializes the table with a corresponding Database.
         /// If no Database is available, no tables can be found.
-        fn init(db: *DB) Self {
-            return Self{ .type = T, .db = db };
+        pub fn init(db: *DB) Self {
+            return Self{ .db = db };
         }
         /// Checks if a table exists
-        fn exists(self: *Self) TableError!bool {
-            self.db.dir.access(@typeName(self.type), .{ .mode = .read_only }) catch return false;
+        pub fn exists(self: *Self) TableError!bool {
+            self.db.dir.access(@typeName(T), .{ .mode = .read_only }) catch return false;
             return true;
         }
         /// Checks if the definition of the table has changed
-        fn changed(self: *Self) !bool {
+        pub fn changed(self: *Self) !bool {
             // TODO
             _ = self;
             return true;
         }
         /// Right now, replacing/updating, deletes all the data
-        fn replace(self: *Self) !void {
-            try self.db.dir.deleteFile(@typeName(self.type));
+        pub fn replace(self: *Self) !void {
+            try self.db.dir.deleteFile(@typeName(T));
             return self.create();
         }
         /// Create The Table on Disk
-        fn create(self: *Self) !void {
+        pub fn create(self: *Self) !void {
             // create file
-            const tbl_file = try self.db.dir.openFile(@typeName(self.type), .{ .mode = .write_only });
+
+            const tbl_file = self.db.dir.openFile(@typeName(T), .{ .mode = .write_only }) catch blk: {
+                break :blk try self.db.dir.createFile(@typeName(T), .{});
+            };
             // initialize row count with 0
-            try tbl_file.write([_]u8{ 0, 0, 0, 0, 0, 0, 0, 0 });
+            _ = try tbl_file.write(&[_]u8{ 0, 0, 0, 0, 0, 0, 0, 0 });
             // write length of colums
-            const fields = @typeInfo(self.type).@"struct".fields;
-            try tbl_file.write([_]u8{@as(u8, fields.len)});
+            const fields = @typeInfo(T).@"struct".fields;
+            _ = try tbl_file.write(&[_]u8{@as(u8, fields.len)});
             // columns
             inline for (fields) |f| {
                 // column length
-                try tbl_file.write([_]u8{@as(u8, f.name.len)});
-                // column type
+                _ = try tbl_file.write(&[_]u8{@as(u8, f.name.len)}); // column type
                 const col_type = DbType.from_type(f.type);
-                try tbl_file.write([_]u8{col_type.to_int()});
+                _ = try tbl_file.write(&[_]u8{col_type.to_int()});
                 // column name
-                try tbl_file.write(f.name);
+                _ = try tbl_file.write(f.name);
             }
         }
     };
@@ -112,7 +113,7 @@ const DbType = enum(u8) {
     t_i128 = 10,
     // string-like
     t_string = 11,
-    fn from_type(comptime T: type) Self {
+    pub fn from_type(comptime T: type) Self {
         return switch (T) {
             u8 => .t_u8,
             u16 => .t_u16,
@@ -128,7 +129,7 @@ const DbType = enum(u8) {
             else => @panic("There is no such Database type"),
         };
     }
-    fn to_type(self: Self) type {
+    pub fn to_type(self: Self) type {
         return switch (self) {
             .t_u8 => u8,
             .t_u16 => u16,
@@ -143,7 +144,7 @@ const DbType = enum(u8) {
             .t_string => []u8,
         };
     }
-    fn to_int(self: Self) u8 {
+    pub fn to_int(self: Self) u8 {
         return @intFromEnum(self);
     }
 };
